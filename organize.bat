18c81cfd485417fad5ab7eabc26462fb6d630bf7
Ian <holmesian17@gmail.com>
2021-07-13 17:48:44 -0600

    Update organize.bat

--- organize.bat
+++ organize.bat
@@ -1,5 +1,5 @@
 @echo off
-for %%i in (*) do (
+for /f "eol=: delims=" %%i in ('dir /b /a-d *^|findstr /live ".bat .txt"') do (
  if not "%%~ni" == "organize"
  (
   md "%%~ni" && move "%%~i" "%%~ni"
