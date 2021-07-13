bf2dba415cb32f705ec673326abc2f6a95bf9153
Ian <holmesian17@gmail.com>
2021-07-13 17:21:31 -0600

    added .bat call

--- pdf_quick_split.py
+++ pdf_quick_split.py
@@ -1,10 +1,16 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
-
+from subprocess import Popen
 import os
 
 # abspath to a folder as a string
 
 folder = os.getcwd()
+print(folder)
+org_file = folder + "\organize.bat"
+print(org_file)
+p = Popen(["organize.bat", org_file])
+stdout, stderr = p.communicate()
+
 
 # inputpdf = PdfFileReader(open(filename, "rb"))
 
@@ -26,4 +32,5 @@ for subdir, dirs, files in os.walk(folder):
                 print(output_name)
                 with open(output_name, "wb") as outputStream:
                     output.write(outputStream)
+
 
