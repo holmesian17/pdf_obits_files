ec2f580723da97f6f5a89e22933a007b516fc6d4
Ian <holmesian17@gmail.com>
2021-07-13 17:24:12 -0600

    ignore the python file

--- organize.bat
+++ organize.bat
@@ -1,6 +1,8 @@
 @echo off
 for %%i in (*) do (
- if not "%%~ni" == "organize" (
+ if not "%%~ni" == "organize"
+ if not "%%~ni" == pdf_quick_split.py"
+ (
   md "%%~ni" && move "%%~i" "%%~ni"
  )
 )
