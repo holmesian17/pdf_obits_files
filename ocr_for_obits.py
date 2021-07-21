a6299bfa675a6f70c315531e6e7e370393b15498
Ian <holmesian17@gmail.com>
2021-07-21 13:50:17 -0600

    Add files via upload

--- ocr_for_obits.py
+++ ocr_for_obits.py
@@ -1,15 +1,15 @@
 from PIL import Image
 import pytesseract
-import argparse
 import cv2
 import os
-
+import argparse
+'''
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-                help="path to input image to be OCR'd")
+	help="path to input image to be OCR'd")
 ap.add_argument("-p", "--preprocess", type=str, default="thresh",
-                help="type of preprocessing to be done")
-args=vars(ap.parse_args())
+	help="type of preprocessing to be done")
+args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -20,17 +20,28 @@ if args["preprocess"] == "thresh":
 
 elif args["preprocess"] == "blur":
 	gray = cv2.medianBlur(gray, 3)
+'''
+folder=os.getcwd()
+for file in folder:
+        for filename in os.listdir():
+                if '.jpg' in filename:
+                        try:
+                                print(filename)
+                                image = cv2.imread(filename)
+                                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                                filename = "{}.png".format(os.getpid())
+                                cv2.imwrite(filename, gray)
 
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
+                                pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-text = pytesseract.image_to_string(Image.open(filename))
-os.remove(filename)
-text_split = text.split()
-print(text_split[0], text_split[1])
-print(text)
+                                text = pytesseract.image_to_string(Image.open(filename))
+                                os.remove(filename)
+                                text_split = text.split()
+                                print(text_split[0:5])
+                                #print(text)
+                        except:
+                                print(filename)
+                                print("Couldn't get name")
 
 # show the output images
 #cv2.imshow("Image", image)
