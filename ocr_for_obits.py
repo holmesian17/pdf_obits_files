4408acf51b7e627cde1ac8c47ebdfdf1ddc28c51
Ian <holmesian17@gmail.com>
2021-07-22 18:47:37 -0600

    Update ocr_for_obits.py

--- ocr_for_obits.py
+++ ocr_for_obits.py
@@ -23,61 +23,65 @@ elif args["preprocess"] == "blur":
 '''
 folder=os.getcwd()
 
-def has_numbers(item):
-        return any(char.isdigit() for char in item)
 
 for file in folder:
         for filename in os.listdir():
                 if '.jpg' in filename:
-                        try:
-                                #print(filename)
-                                image = cv2.imread(filename)
-                                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                                filename = "{}.png".format(os.getpid())
-                                cv2.imwrite(filename, gray)
+                        
+                        #print(filename)
+                        image = cv2.imread(filename)
+                        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                        filename = "{}.png".format(os.getpid())
+                        cv2.imwrite(filename, gray)
 
-                                pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+                        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-                                text = pytesseract.image_to_string(Image.open(filename))
-                                os.remove(filename)
-                                text_split = text.split()
-                                first_ten_words = text_split[0:10]
-                                first_ten_words = list(set(first_ten_words))
-                                print(first_ten_words)
+                        text = pytesseract.image_to_string(Image.open(filename))
+                        os.remove(filename)
+                        text_split = text.split()
+                        first_ten_words = text_split[0:10]
+                        print("Original ten: ", first_ten_words)
+                        first_ten_words = list(set(first_ten_words))
+                        print("Duplicate words removed: ", first_ten_words)
+                        
+                        for item in first_ten_words:
+                                print(item)
+                                if '"' in item:
+                                        print("Has quotes: ", item)
+                                        first_ten_words.remove(item)
+                                elif item.isnumeric() == True:
+                                        print("This is a number: ", item)
+                                        first_ten_words.remove(item)
+                                elif any(chr.isdigit() for chr in item) == True:
+                                        print("Has digits: ", item)
+                                        first_ten_words.remove(item)
+                        print("Final result: ", first_ten_words)
                                 
-                                for item in first_ten_words:
-                                        if '"' in item:
-                                                print(item)
-                                                first_ten_words.remove(item)
-                                        elif has_numbers(item) == True:
-                                                print(item)
-                                                first_ten_words.remove(item)
-                                                
-                                
-                                
-                                first_name = open("C:\\Documents\first_names.txt", "r")
-                                last_name = open("C:\\Documents\last_names.txt", "r")
-                                
-                                first_names_list = []
-                                last_names_list = []
-                                fullname = []
-                                
-                                for item in first_ten_words:
-                                        if item in first_name:
-                                                first_names_list.add(item)
-                                        elif item in last_name:
-                                                last_names_list.add(item)
-                                        else:
-                                                pass
-                                if item.startswith("B") in last_names_list:
-                                        fullname.append[item]
+                        
+                        '''
+                        first_name = open("C:\\Documents\first_names.txt", "r")
+                        last_name = open("C:\\Documents\last_names.txt", "r")
+                        
+                        first_names_list = []
+                        last_names_list = []
+                        fullname = []
+                        
+                        for item in first_ten_words:
+                                if item in first_name:
+                                        first_names_list.add(item)
+                                elif item in last_name:
+                                        last_names_list.add(item)
                                 else:
                                         pass
-                                        
+                        if item.startswith("B") in last_names_list:
+                                fullname.append[item]
+                        else:
+                                pass
+                                     
                         except:
                                 print(filename)
                                 print("Couldn't get text. Manually rename.")
-
+                        '''
 # show the output images
 #cv2.imshow("Image", image)
 #cv2.imshow("Output", gray)
