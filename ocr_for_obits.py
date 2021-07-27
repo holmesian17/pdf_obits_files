ebe4f98d2798aa59d002d6ac1df86f5a2c242df0
Ian <holmesian17@gmail.com>
2021-07-26 19:11:12 -0600

    Update ocr_for_obits.py

--- ocr_for_obits.py
+++ ocr_for_obits.py
@@ -39,28 +39,55 @@ for file in folder:
                         os.remove(filename)
                         text_split = text.split()
                         first_ten_words = text_split[0:10]
-                        print("Original ten: ", first_ten_words)
+                        #print("Original ten: ", first_ten_words)
                         first_ten_words = [i.replace('"',  '~') for i in first_ten_words]
                         first_ten_words = [i.replace('”',  '~') for i in first_ten_words]
                         first_ten_words = [i.replace('“',  '~') for i in first_ten_words]
                         first_ten_words = list(set(first_ten_words))
-                        #print("Duplicate words removed: ", first_ten_words)
+                        print("Duplicate words removed: ", first_ten_words)
+
+
+                        first_ten_words = [item.capitalize() for item in first_ten_words]
+                        first_ten_words = [item for item in first_ten_words if '~' not in item]
+                        #print("No tildes: ", first_ten_words)
+                        first_ten_words = [item for item in first_ten_words if item.isnumeric() == False]
+                        #print("NaN: ", first_ten_words)
+                        first_ten_words = [item for item in first_ten_words if any(chr.isdigit() for chr in item) == False]
+                        #print("Na(ny)N: ", first_ten_words)
+                        first_ten_words = [item for item in first_ten_words if 'of' not in item]
+                        first_ten_words = [item for item in first_ten_words if 'and' not in item]
+                        first_ten_words = [item for item in first_ten_words if 'the' not in item]
+                        first_ten_words = [item for item in first_ten_words if 'Fort' not in item]
+
+
+                        first_names_list = []
+                        last_names_list = []
+                        fullname = []
+
 
                         for item in first_ten_words:
-                                item=item.lower()
-                                item=item.capitalize()
+                                with open ('first_names.txt') as search:
+                                        for line in search:
+                                                line = line.rstrip()
+                                                if item == line:
+                                                        first_names_list.append(item)
+                        for item in first_ten_words:
+                                with open ('last_names.txt') as search:
+                                        for line in search:
+                                                line = line.rstrip()
+                                                if item == line:
+                                                        last_names_list.append(item)
+                                                
                                 
-                        first_ten_words = [item for item in first_ten_words if '~' not in item]
-                        print("No tildes: ", first_ten_words)
-                        first_ten_words = [item for item in first_ten_words if item.isnumeric() == False]
-                        print("NaN: ", first_ten_words)
-                        first_ten_words = [item for item in first_ten_words if any(chr.isdigit() for chr in item) == False]
-                        print("Na(ny)N: ", first_ten_words)
+                        print("Last name: ", last_names_list)
+                        print("First name: ", first_names_list)
+                                        
+                        if item.startswith("B") in last_names_list:
+                                fullname.append[item]
+                        else:
+                                pass
                         
-                        
-                                
-                        
-                  
+                        '''
                         first_name = open("C:\\Documents\first_names.txt", "r")
                         last_name = open("C:\\Documents\last_names.txt", "r")
                         
@@ -83,8 +110,9 @@ for file in folder:
                         except:
                                 print(filename)
                                 print("Couldn't get text. Manually rename.")
-                        '''
+                        
 # show the output images
 #cv2.imshow("Image", image)
 #cv2.imshow("Output", gray)
 #cv2.waitKey(0)
+'''
